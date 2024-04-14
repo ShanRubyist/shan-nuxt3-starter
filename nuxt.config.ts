c74c3b5ba27a4d29c0a63b23f3377b1d1c2e2eca
shan <2302531309@qq.com>
2024-04-14 15:45:45 +0800

    feat: 集成 nuxt ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: ["@nuxt/ui"],
+
+  colorMode: {
+    preference: 'system'
+  },
+
   devtools: { enabled: true }
 })
